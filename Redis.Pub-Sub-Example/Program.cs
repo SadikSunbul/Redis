d3cff51b92e2ed944b117ef04e0f2e6a2618ff65
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-31 16:32:54 +0300

    redis.Sub/Pub eklendi

---
+++ Redis.Pub-Sub-Example/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1452");//redise bağlantımız sunucuda sıfre vb seyler var ıse opt=>{} ile içerisinde halledin
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync("mychanel", message);
+}
---
+++ Redis.Pub-Sub-Subscribe-Example/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1452");//redise bağlantımız sunucuda sıfre vb seyler var ıse opt=>{} ile içerisinde halledin
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+//bası mychanel sonu onemsız bırsekılde olanlardan okuycaktır verıyı 
+await subscriber.SubscribeAsync("mychanel.*", (channel, mesage) =>
+{
+    Console.WriteLine(mesage);
+});
+
+Console.ReadLine();
